add2efc9cbf7fe3eca084334b464401ee7bec69e
rickardinho <d.rickardinho@gmail.com>
2017-07-11 00:06:34 +0100

    Update app.jsx

--- src/js/components/app.jsx
+++ src/js/components/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from './general/navbar.jsx';
 import Message from './general/message.jsx';
-import { connectAlert } from './Alert';
 
 const App = ({ location, children, error }) => {
 
@@ -14,17 +13,11 @@ const App = ({ location, children, error }) => {
 
     let online = window.navigator.onLine;
     
-    renderAlert = () => {
-        setTimeout(() => {
-          this.props.alertWithType('error', 'No connection', 'You are not connected to Internet!');
-        }, 2000);
-    }
-    
     return (
         <div>
             {
-                !online && this.renderAlert()
-                // <Message extraClass="offline" text="Oops! No internet connection..." />
+                !online &&
+                <Message extraClass="offline" text="Oops! No internet connection..." />
                 
             }
             {
