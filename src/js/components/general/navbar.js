f09264e9218502636c5a09ae0e68555dc207ef70
rickardinho <d.rickardinho@gmail.com>
2017-07-11 00:12:11 +0100

    Update navbar.js

--- src/js/components/general/navbar.js
+++ src/js/components/general/navbar.js
@@ -20,18 +20,6 @@ const Navbar = ({ currentLocation }) => {
         "selected": currentLocation.indexOf("/calendar") > -1
     });
 
-    let feedClasses = classnames("nav-button", {
-        "selected": currentLocation.indexOf("/feed") > -1
-    });
-
-    let profileClasses = classnames("nav-button", {
-        "selected": currentLocation.indexOf("/profile") > -1
-    });
-
-    let createEventClasses = classnames("nav-button", {
-        "selected": currentLocation.indexOf("/create-event") > -1
-    });
-
 
     return (
         <div className={ navbarClasses }>
@@ -49,26 +37,6 @@ const Navbar = ({ currentLocation }) => {
                 </div>
             </Link>
 
-            <Link className={ feedClasses } to="feed">
-                <div >
-                    <i className="fa fa-globe nav-icon"></i>
-                    <label className="menu-label">Feed</label>
-                </div>
-            </Link>
-
-            <Link className={ profileClasses } to="profile">
-                <div >
-                    <i className="fa fa-user nav-icon"></i>
-                    <label className="menu-label">Profile</label>
-                </div>
-            </Link>
-
-            <Link className={ createEventClasses } to="create-event">
-                <div >
-                    <i className="fa fa-pencil nav-icon"></i>
-                    <label className="menu-label">Create</label>
-                </div>
-            </Link>
         </div>
     );
 };
