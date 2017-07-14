257bf02e6c839557e73857517fd0de2818b316e9
rickardinho <d.rickardinho@gmail.com>
2017-07-14 08:24:46 +0100

    commitCreateReactApp

--- test/server/lib/getEndTime.js
+++
@@ -1,9 +0,0 @@
-import test from 'tape';
-import getEndTime from '../../../src/js/lib/getEndTime.js';
-
-test('getEndTime takes a date and returns the next day at 6am', (t) => {
-
-    const start = "2015-05-24";
-    // const result = getEndTime(start);
-    t.end();
-});
--- test/unit/actions/fixtures.js
+++
@@ -1,8 +0,0 @@
-export const event = {
-
-    RSVP: {
-        going: [],
-        notGoing: [],
-        maybe: ['12345678']
-    }
-};
