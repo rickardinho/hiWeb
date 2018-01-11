d67ababf824ce97c49bb454d48b9d62a42c5dff0
rickardinho <d.rickardinho@gmail.com>
2018-01-11 13:45:01 +0000

    commitBPlate2

--- src/js/components/login.js
+++ src/js/components/login.js
@@ -4,7 +4,14 @@ import { PureGridProvider } from 'react-pure-grid';
 
 const Login = () => (
   <div>
-
+    <div className="App">
+      <div className="App-header">
+        <h2>Welcome to Hi Web</h2>
+      </div>
+      <p className="App-intro">
+        To get started, blah blah.
+      </p>
+    </div>
   </div>
 );
 
